fa6da1b83d3520c96e1699e357333ac1e9e935f7
itu-itis24-tuibekov23 <tuibekov23@itu.edu.tr>
2025-05-11 15:24:31 +0300

    Создана структура навигации и экраны Home, Statistics, NotFound

---
+++ mobile-app/mobile-app/app/stats.tsx
@@ -0,0 +1,21 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function StatsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>This is the Statistics Screen 📊</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: '600',
+  },
+});
